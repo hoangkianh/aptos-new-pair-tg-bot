17cd3dc79a03d6268fb27307c652fd1964b09a77
Anh Hoang Nguyen <hoangkianh31592@gmail.com>
2024-05-03 11:00:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,3 +35,9 @@ const pendingTransaction = await aptos.transaction.submit.simple({
   transaction,
   senderAuthenticator,
 });
+
+// using signAndSubmit combined
+const pendingTransaction = await aptos.signAndSubmitTransaction({
+  signer: alice,
+  transaction,
+});
